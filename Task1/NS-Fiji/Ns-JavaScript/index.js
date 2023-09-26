cf0192f2198b336cb9a0a833ab8cd7e21ac09819
Ankita <ankitakumawat111@gmail.com>
2023-09-26 17:20:32 +0530

    initial commit

---
+++ Task1/NS-Fiji/Ns-JavaScript/index.js
@@ -0,0 +1,27 @@
+// let sum = 0;
+// let n = prompt("Enter the number");
+// a = Number.parseInt(n)
+// for (let i = 1; i <= n; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
+// let object = {
+//     harry: 50,
+//     Marry: 60,
+//     Tarry: 70,
+//     carry: 80,
+//     Sarry: 90,
+// }
+// for (let a of "Ankita") {
+//     console.log("Marks of " + a + " are " + object[a]);
+// }
+// arrow function
+// 
+
+let cn = 4;
+let i;
+while (i != cn) {
+    i = prompt("Enter the  number");
+}
+console.log("You have entered correct number " + i);
